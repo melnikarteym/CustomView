4b7778c7d9c5c8285cd0ecce2e0fc1e89eb5ba57
Artem M <a.melnik@gmail.com>
2022-11-15 00:34:48 +0300

    improve

--- app/src/main/java/com/example/customview/profile_header/ProfileHeader.kt
+++ app/src/main/java/com/example/customview/profile_header/ProfileHeader.kt
@@ -2,11 +2,15 @@ package com.example.customview.profile_header
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.setPadding
 import com.example.customview.R
+import com.example.customview.dpToPx
 
 class ProfileHeader @JvmOverloads constructor(
     context: Context,
@@ -18,6 +22,11 @@ class ProfileHeader @JvmOverloads constructor(
     private lateinit var userName: TextView
 
     init {
+        /*
+        //magic
+        gravity = Gravity.CENTER
+        setPadding(PADDING_IN_DP.dpToPx(context))
+        */
         val inflatedView = inflate(context, R.layout.profile_header, this)
         initViews(inflatedView)
         setupViewsIfNeeded(context, attrs)
@@ -39,6 +48,11 @@ class ProfileHeader @JvmOverloads constructor(
             avatar.setImageDrawable(getDrawable(R.styleable.ProfileHeader_profileAvatar))
             userName.text = getString(R.styleable.ProfileHeader_profileUserName)
         }
+    }
+
+    private companion object {
+
+        const val PADDING_IN_DP = 8
     }
 }
 
